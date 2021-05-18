7fcccc27db79efcb13f2fb2a53f0e9e0a320dbfb
Sina Shirinpour <32249508+Sinaxist@users.noreply.github.com>
2021-05-18 15:52:11 -0500

    Update Aberra_L5_model.m

--- Model_Generation/Aberra_L5_model.m
+++ Model_Generation/Aberra_L5_model.m
@@ -375,8 +375,8 @@ delete('./lib_custom/*');
 rmdir('lib_custom');
 delete('./lib_genroutines/*');
 rmdir('./lib_genroutines/');
-delete('./lib_mech/*');
-rmdir('./lib_mech/');
+% delete('./lib_mech/*');
+rmdir('./lib_mech/', 's');
 rmpath('./Aberra_files/');
 
 end
