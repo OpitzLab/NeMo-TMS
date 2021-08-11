47e96c0bdf2cdf9be0b9e1e3dbb9e11986204f88
Sina Shirinpour <32249508+Sinaxist@users.noreply.github.com>
2021-08-11 13:15:09 -0500

    Add files via upload
    Added stack for save_locations.hoc

--- Model_Generation/Aberra_files/add_stack.m
+++ Model_Generation/Aberra_files/add_stack.m
@@ -1,6 +1,7 @@
 function add_stack(name)
 if (ispc == 1)
-    filename = strcat('../Models/', name, '/Code/NEURON/mosinit.ps1');
+    filename = strcat('../Models/', name, '/Code/NEURON/TMS_script.ps1');
+    filename2 = strcat('../Models/', name, '/Code/NEURON/save_locations.ps1');
     [~,outp] = system('where nrniv');
     if isempty(outp) || ~isempty(regexp(outp,'not found','ONCE'))  || ~isempty(regexp(outp,'no nrniv','ONCE')) || ~isempty(regexp(outp,'not find','ONCE')) || strcmp(outp,sprintf('\n')) || isempty(regexp(outp,'nrniv','ONCE'))
         t2npath = fileparts(which('t2n.m'));
@@ -11,10 +12,14 @@ if (ispc == 1)
         nrnivPath = 'nrniv';
     end
 else
-    filename = strcat('../Models/', name, '/Code/NEURON/mosinit.sh');
+    filename = strcat('../Models/', name, '/Code/NEURON/TMS_script.sh');
+    filename2 = strcat('../Models/', name, '/Code/NEURON/save_locations.sh');
     nrnivPath = 'nrniv';
 end
 
 fileID = fopen(filename,'w');
 fprintf(fileID,[nrnivPath ' -NSTACK 100000 -NFRAME 20000 -Py_NoSiteFlag TMS_script.hoc']);
+fclose(fileID);
+fileID = fopen(filename2,'w');
+fprintf(fileID,[nrnivPath ' -NSTACK 100000 -NFRAME 20000 -Py_NoSiteFlag save_locations.hoc']);
 fclose(fileID);
